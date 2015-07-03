7a38c3b427fafccf3a4d53011d97ec7b506328f4
nghialv <levan_nghia@cyberagent.co.jp>
2015-07-03 12:13:15 +0900

    prototype

--- VersionTracker/VersionTracker.swift
+++ VersionTracker/VersionTracker.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+
+private let VersionTrailKey = "VersionTracker.VersionTrailKey"
+private let VersionsKey = "VersionTracker.VersionsKey"
+private let BuildsKey = "VersionTracker.BuildsKey"
+
+public struct VersionTracker {
+	typealias VersionTrailDictionary = [String: [String]]
+	
+	private static var versionTrail: VersionTrailDictionary	= [:]
+	public private(set) static var isFirstLaunchEver		= false
+	public private(set) static var isFirstLaunchForVersion	= false
+	public private(set) static var isFirstLaunchForBuild	= false
+	
+	public static var currentVersion: String {
+		return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
+	}
+	
+	public static var currentBuild: String {
+		return NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleVersion") as! String
+	}
+	
+	public static func track() {
+		let standardUserDefaults = NSUserDefaults.standardUserDefaults()
+		
+		let oldVersionTrail = standardUserDefaults.objectForKey(VersionTrailKey) as? VersionTrailDictionary
+		
+		if let oldVersionTrail = oldVersionTrail {
+			isFirstLaunchEver = false
+			versionTrail = oldVersionTrail
+		} else {
+			isFirstLaunchEver = true
+			versionTrail[VersionsKey] = []
+			versionTrail[BuildsKey] = []
+		}
+		
+		if let versions = versionTrail[VersionsKey] where contains(versions, currentVersion) {
+			isFirstLaunchForVersion = false
+		} else {
+			isFirstLaunchForVersion = true
+			var versions = versionTrail[VersionsKey] ?? []
+			versions.append(currentVersion)
+			versionTrail[VersionsKey] = versions
+		}
+		
+		if let builds = versionTrail[BuildsKey] where contains(builds, currentBuild) {
+			isFirstLaunchForBuild = false
+		} else {
+			isFirstLaunchForBuild = true
+			var builds = versionTrail[BuildsKey] ?? []
+			builds.append(currentBuild)
+			versionTrail[BuildsKey] = builds
+		}
+		
+		// Store version trail
+		standardUserDefaults.setObject(versionTrail, forKey: VersionTrailKey)
+		standardUserDefaults.synchronize()
+	}
+}
