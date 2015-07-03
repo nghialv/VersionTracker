82b4dffc7b20ea1f354ca41eef9621af6e0dc28d
nghialv <levan_nghia@cyberagent.co.jp>
2015-07-03 11:35:07 +0900

    init

---
+++ Example/VersionTracker/VersionTracker.h
@@ -0,0 +1,19 @@
+//
+//  VersionTracker.h
+//  VersionTracker
+//
+//  Created by Le VanNghia on 7/3/15.
+//  Copyright (c) 2015 Le Van Nghia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VersionTracker.
+FOUNDATION_EXPORT double VersionTrackerVersionNumber;
+
+//! Project version string for VersionTracker.
+FOUNDATION_EXPORT const unsigned char VersionTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VersionTracker/PublicHeader.h>
+
+
---
+++ VersionTracker/VersionTracker.swift
@@ -0,0 +1,9 @@
+//
+//  VersionTracker.swift
+//  Example
+//
+//  Created by Le VanNghia on 7/3/15.
+//  Copyright (c) 2015 Le Van Nghia. All rights reserved.
+//
+
+import Foundation
